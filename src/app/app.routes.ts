c3718ff5929caa48f598dcf1f01e2b01839809cb
Frank Jerez <admin@frankdev172.onmicrosoft.com>
2025-11-19 14:41:04 -0500

    more ui changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
-import { LanceOasisCopilotComponent } from './hello/lance-oasis-copilot';
+import { LanceOasisCopilotComponent } from './oasis/lance-oasis-copilot';
+import { AboutComponent } from './about/about.component';
+import { LanceLoginAndListComponent } from './login-and-list/login-and-list';
 
 export const routes: Routes = [
-  { path: '', component: LanceOasisCopilotComponent },
+  { path: 'oasis', component: LanceOasisCopilotComponent },
+  { path: '', component: LanceLoginAndListComponent },
+  { path: 'about', component: AboutComponent },
   {
     path: '**',
     redirectTo: '',
